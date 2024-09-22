fa9710933725266d23cd0deeed6de050f20816d1
Tom Wieland <tom.wieland@gmail.com>
2024-09-22 18:26:59 +0200

    refactor: add more properties and documentation

--- lib/devshells.nix
+++ lib/devshells.nix
@@ -1,12 +1,20 @@
-{packages}: {
+{
+  # packages: The packages to add to the devshell.
+  packages ? [],
+  # buildInputs: The build inputs to add to the devshell.
+  buildInputs ? [],
+  # inputsFrom: The inputs to add to the devshell.
+  inputsFrom ? [],
+  # shellHook: The shell hook to add to the devshell.
+  shellHook ? "",
+}: {
   pkgs,
   self,
   system,
 }: {
   default = pkgs.mkShell {
-    inherit (self.checks.${system}.pre-commit-check) shellHook;
-    buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
-    inputsFrom = [];
-    inherit packages;
+    inherit packages inputsFrom;
+    shellHook = shellHook // self.checks.${system}.pre-commit-check.shellHook;
+    buildInputs = buildInputs // self.checks.${system}.pre-commit-check.enabledPackages;
   };
 }
